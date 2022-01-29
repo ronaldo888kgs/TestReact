7588b356753ff2b04a5cfcca7674a52756d203c8
GuoJin <82759161+ronaldo888kgs@users.noreply.github.com>
2022-01-29 17:09:53 +0800

    userlist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import "./styles.css";
+import React, { useRef } from "react";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
-export default function App() {
+function App() {
+  const users = [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com"
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com"
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com"
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <CreateUser />
+      <UserList users={users} />
+    </>
   );
 }
+
+export default App;
